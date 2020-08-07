4491d44670827819a5b9c69b3233958f99da44a6
MentalG <gromovthefirst@gmail.com>
2020-08-07 17:29:23 +0300

    commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import image from '../../materials/purple.png';
+
+function App() {
+  // const myImageData = 
+
+  return (
+    <div className="App">
+      <div>123</div>
+      <canvas onLoad={() => console.log('loaded')}></canvas>
+    </div>
+  );
+}
+
+export default App;
